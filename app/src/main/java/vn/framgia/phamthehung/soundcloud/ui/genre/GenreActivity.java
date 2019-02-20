c3f92b94ba5efee99809faf8d14f987f29a89c66
Hiepsisoma <phamthehung212@gmail.com>
2019-02-20 18:47:25 +0700

    create_ui_genre

--- app/src/main/java/vn/framgia/phamthehung/soundcloud/ui/genre/GenreActivity.java
+++ app/src/main/java/vn/framgia/phamthehung/soundcloud/ui/genre/GenreActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import vn.framgia.phamthehung.soundcloud.R;
 import vn.framgia.phamthehung.soundcloud.data.model.Genre;
@@ -14,8 +16,8 @@ public class GenreActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_genre);
+        initActionBar();
     }
 
     public static Intent getIntent(Context context, Genre genre) {
@@ -26,4 +28,23 @@ public class GenreActivity extends AppCompatActivity {
         return intent;
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_genre, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    public void initActionBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.null_string);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
 }
--- app/src/main/java/vn/framgia/phamthehung/soundcloud/ui/main/MainActivity.java
+++ app/src/main/java/vn/framgia/phamthehung/soundcloud/ui/main/MainActivity.java
@@ -1,12 +1,17 @@
-package vn.framgia.phamthehung.soundcloud;
+package vn.framgia.phamthehung.soundcloud.ui.main;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import vn.framgia.phamthehung.soundcloud.R;
 import vn.framgia.phamthehung.soundcloud.ui.discover.DiscoverFragment;
 import vn.framgia.phamthehung.soundcloud.ui.personal.PersonalFragment;
 import vn.framgia.phamthehung.soundcloud.ui.setting.SettingFragment;
@@ -18,11 +23,11 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
         BottomNavigationView mBottomNavigationView = findViewById(R.id.bottom_navigation);
         mBottomNavigationView.setOnNavigationItemSelectedListener(this);
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, new DiscoverFragment()).commit();
+        checkInternetConnection();
     }
 
     @Override
@@ -41,6 +46,27 @@ public class MainActivity extends AppCompatActivity
         }
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_container, fragment).commit();
+        return true;
+    }
+    private boolean checkInternetConnection() {
+        ConnectivityManager connManager = (ConnectivityManager) this
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connManager.getActiveNetworkInfo();
+
+        if (networkInfo == null) {
+            Toast.makeText(this, getString(R.string.msg_no_network), Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        if (!networkInfo.isConnected()) {
+            Toast.makeText(this, getString(R.string.msg_not_connected), Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        if (!networkInfo.isAvailable()) {
+            Toast.makeText(this, getString(R.string.msg_not_available), Toast.LENGTH_LONG).show();
+            return false;
+        }
         return true;
     }
 
